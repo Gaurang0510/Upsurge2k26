22aa60919fef7e1c7c965512a0bdd628ec12af50
Prasanna Lohe <loheprasanna@gmail.com>
2026-07-26 10:46:49 +0530

    fixed the scroll animations and ps layout for mobile

--- frontend/src/components/hackathon/TrackDetailModal.jsx
+++ frontend/src/components/hackathon/TrackDetailModal.jsx
@@ -157,24 +157,3 @@ export default function TrackDetailModal({ track, onClose }) {
   );
 }
 
-// Inline fallback icons for specific domains
-function HeartPulseIcon(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"/>
-      <path d="M3.22 12H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27"/>
-    </svg>
-  );
-}
-
-function SproutIcon(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
-      <path d="M7 20h10"/>
-      <path d="M10 20c5.5-2.5.8-6.4 3-10"/>
-      <path d="M9.5 9.4c1.1.8 1.8 2.2 2.3 3.7-1.5-.4-3.2-.4-4.8.3-1.2.6-2.3 1.6-3 2.8 3-.4 6-2 5.5-6.8"/>
-      <path d="M14.1 6a7 7 0 0 0-1.1 4c1.9-.1 3.7-.6 5.2-1.7 1.2-.9 2-2.2 2.3-3.7-3.1.2-5.7 1.6-6.4 1.4"/>
-    </svg>
-  );
-}
-
